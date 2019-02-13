e16e11776f402c56798afdf31f2e513cb860f791
Lemar Popal <lemarpopal@Lemars-MacBook-Pro.local>
2019-02-12 16:58:52 -0800

    first commit

---
+++ Course Materials - Student/In-Class Activities/3_Activity_Script.R
@@ -0,0 +1,28 @@
+divide_and_round <- function(nums){
+  
+  my_mean <-1
+  divided <- nums/my_mean
+  rounded <- 0
+  
+  rounded
+  
+}
+
+
+every_other <- function(vec, start = 1){
+  
+  new_vec <- c()
+  
+  for(val in vec){
+    
+    if(){
+      
+      new_vec <- c(new_vec, val)
+      
+    }
+    
+  }
+  
+  new_vec
+  
+}
